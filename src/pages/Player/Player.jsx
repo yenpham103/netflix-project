5b2deef05276f82faf7bad1828f6c1945c5d0e6b
phamyen1003 <nhovequehuong1003@gmail.com>
2024-07-06 20:32:49 +0700

    update

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -30,20 +30,20 @@ function Player() {
 
   //
   return (
-    <div className="player" key={apiData.key}>
+    <div className="player" key={apiData?.key}>
       <img src={back_arrow_icon} alt="" onClick={() => navigate(-2)} />
       <iframe
         width="90%"
         height="90%"
-        src={`https://www.youtube.com/embed/${apiData.key}`}
+        src={`https://www.youtube.com/embed/${apiData?.key}`}
         title="trailer"
         frameBorder="0"
         allowFullScreen
       ></iframe>
       <div className="player-info">
-        <p>{apiData.published_at.slice(0, 10)}</p>
-        <p>{apiData.name}</p>
-        <p>{apiData.type}</p>
+        <p>{apiData?.published_at.slice(0, 10)}</p>
+        <p>{apiData?.name}</p>
+        <p>{apiData?.type}</p>
       </div>
     </div>
   );
